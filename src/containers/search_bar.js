f3cf456186e0693ea944bdca17ac18df1eb5d7f7
Raj Singh <raj@foreversingh.com>
2017-06-19 11:09:50 -0500

    bound promise handling and fetchWeather action to search bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
 
-export default class SearchBar extends Component {
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
 
-    this.onInputChange = this.onInputChange.bind(this)
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(e) {
@@ -16,7 +21,8 @@ export default class SearchBar extends Component {
   onFormSubmit(e) {
     e.preventDefault();
     // Go fetch weather data
-    // api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' })
   }
 
   render() {
@@ -37,3 +43,9 @@ export default class SearchBar extends Component {
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
